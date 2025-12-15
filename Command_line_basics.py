49a5d209df2cc25dfaac27a6453d681c2c87a387
nuranaalili <nurana.alili@outlook.com>
2025-12-15 11:07:43 -0800

    UW projects

---
+++ Command_line_basics.py
@@ -0,0 +1,21 @@
+# Print the current working directory.
+pwd
+
+# List the contents of the current directory.
+ls
+
+# Change directory into the `data/` directory.
+cd data
+
+# Use the `head` command to view the first 10 lines of the
+# `seuss.txt` file found in the `data/` directory.
+head seuss.txt
+
+# Change directory back to the `home/` directory (the one
+# that contains the `data/` folder).
+# DO NOT use an absolute path!
+cd ..
+# WITHOUT CHANGING DIRECTORIES, use the `wc` command to
+# count the number of  lines, words, and characters in the
+# `seuss.txt` file found in the `data/` directory.
+wc data/seuss.txt
